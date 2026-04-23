cf95525501e0dd73b9fe6ac833fb3f0b803a7707
Kelly Boucher <kelly_boucher@hotmail.com>
2026-04-23 10:21:13 -0400

    Ajout exercice 2

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+choix=
+
+while [[ $choix != q ]]
+do
+    clear #nettoie l'écran
+    echo "==================="
+    echo " - LinuxSys v1.0  -"
+    echo "  m = dictionnaire "
+    echo "  q = quitter      "
+    echo "-------------------"
+    echo "Entrez q pour quitter, m pour exécuter exercice 1"
+    read -p "Entrez un choix" choix
+
+    if[[ $choix = "m" ]]
+    then
+    #Demander un mot au clavier
+        read -p "Quel mot voulez-vous vérifier ? " mot
+        
+    #Appeler exercice 1
+        ./exercice1.sh $mot
+
+    #Attendre 3 secondes
+    sleep 3
+    fi
+done
+
