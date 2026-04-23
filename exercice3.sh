2c1e7487420e0590d4581e170edcadf8eca2e200
Kelly Boucher <kelly_boucher@hotmail.com>
2026-04-23 10:41:26 -0400

    Ajout exercice 3

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# Étape 1 = faire une fonction qui prend un param
+# Objectif = faire un nombre d'espaces en fonction du param
+# ex: faireEspace 4 -> echo "    "
+#Utiliser une boucle while et non pas for, car $i dans la for la fait crasher
+
+# Étape 2 = Faire une boucle de 40 itérations, qui : 
+# 1- Appelle faireEspace avec le $i
+
+# 2- Ajouter le caractère @ (la petite bombe)
+# 3- Ajouter un sleep de 0.1
+# 4- Ajouter un clear
+
+# Étape 3 (on est sorti de la boucle)
+# Afficher "BOOM"
+
