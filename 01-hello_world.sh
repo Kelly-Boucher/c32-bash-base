b77f05190d8b186b4cce88965168c2e79c13f5ed
Kelly Boucher <kelly_boucher@hotmail.com>
2026-04-23 08:59:27 -0400

    Ajout de notes de cours

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+#pour exécuter un fichier, soit ./ ou /root/linus/ etc (chemin absolu)
+
+#exit
+echo "BOOM" 
+#boom n'apparaitra pas au terminal parce que exit termine l'exécution avant
+
+#ctrl+é pour mettre en commentaire n'importe quoi
+echo -n "Vive " #-n empêche le retour de ligne par défaut qui vient avec echo
+echo "Linux"
+
+#Évalue les méta-caractères
+echo -e "123\n\tn456" 
+
+#Équivalent du cin en c++, pas de $ quand on affecte la variable, juste en lecture,
+ read -p "Quel âge avez-vous?" age 
+echo $age "ans!"
