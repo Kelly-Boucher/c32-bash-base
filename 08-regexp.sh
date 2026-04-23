63a87f46c65284b349e042c59b081a9130da7b90
Kelly Boucher <kelly_boucher@hotmail.com>
2026-04-23 09:53:59 -0400

    Ajout des regex

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+echo -n "Texte : " #-n pour pas faire de fin de ligne
+read choix
+#même chose que :
+read -p "Texte : " choix
+
+#* optionnel, mais peut en avoir plusieurs
+# + au moins un jusqu'à l'infini (au moins 1!)
+if [[ $choix =~ ^[0-9]+$ ]] #le tilde veut dire que c'est un motif, le plus veut dire à l'infini jusqu'à la fin ($)
+then    
+    echo "nombre"   #on affiche "nombre" à la console seulement si la personne a entré un nombre
+fi
+
