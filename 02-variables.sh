ed21f3a2596c2d7b4830d5cc5bf857af4f66319d
Kelly Boucher <kelly_boucher@hotmail.com>
2026-04-23 09:07:21 -0400

    Notes sur les variables avec echo

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+#l'expace est nécessaire pour l'assignation
+prenom=John
+echo $prenom
+
+#pour être sur qu'on déclare un chiffre (i pour integer)
+declare -i age=44
+declare -r nom=Smith    #lecture seule, on peut pas le changer, comme une constante
+
+echo $age
+
+echo $0 #Affiche le nom du fichier
+echo $1 #Affiche le 2e paramètre de la commande qui vient d'être entré dans le terminal bash
+#$1 à $9 = paramètre du script
+echo $# # Combien de paramètres ont été passés
