7574711fef101f591ed890fa1a81471a3ebb3e26
Kelly Boucher <kelly_boucher@hotmail.com>
2026-04-23 09:10:58 -0400

    Calculs

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+echo 10+10 #Affiche littéralement 10+10
+echo $((10+10)) #sans espace!!!
+
+read -p "Entrez un nombre" nb1
+read -p "Un autre nombre" nb2
+
+let resultat=$nb1*nb2
+echo $resultat
