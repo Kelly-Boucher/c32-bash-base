8f66963b9f011775efd9c8062e90ef57ba7a87c2
Kelly Boucher <kelly_boucher@hotmail.com>
2026-04-23 09:12:32 -0400

    Ajout d'un commentaire

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -6,5 +6,5 @@ echo $((10+10)) #sans espace!!!
 read -p "Entrez un nombre" nb1
 read -p "Un autre nombre" nb2
 
-let resultat=$nb1*nb2
+let resultat=$nb1*nb2 #declare -i est similaire mais let fait le calcul et fait pas juste une déclaration de variable, let est vraiment pour les chiffres
 echo $resultat
