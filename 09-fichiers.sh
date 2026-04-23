3128d327bb6ddd0d081a3b5e6bc35be2519679a1
Kelly Boucher <kelly_boucher@hotmail.com>
2026-04-23 09:57:57 -0400

    Ajout des notes sur les fichiers

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+#alt + flèche gauche ou flèche roite pour se promener entre les différentes sections où on était
+
+if test -e "LICENSE" #le e veut dire "existe", -d veut dire "si c'est un répertoire"
+then    
+    echo "Le fichier existe"
+fi
+
+if test -e $1 #fait la même chose, mais pas hard codé
+then    
+    echo "Le fichier existe"
+fi
+
