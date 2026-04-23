3f4aba574bc8e6af5bc40021943c7b5ff07fb385
Kelly Boucher <kelly_boucher@hotmail.com>
2026-04-23 09:40:54 -0400

    Ajout des boucles

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+for ((i=0; i<5; i++)) #-lt ne marcherait pas for some reason...
+do
+    echo $i
+done
+
+#listeFichier=ls équivalent à listeFichier="ls"
+
+listeFichier=`ls` #met le résultat de la commande dans la variable listeFichier et n'affiche rien à la console
+
+for fichier in $listeFichier #va afficher tous les noms des fichiers dans le répertoire courant à cause de la commande ls
+do
+    echo $fichier
+done
+
+choix=
+
+while [[ $choix != q ]]
+do
+    clear #nettoie l'écran
+    echo "==========="
+    echo "- Menu 1 -"
+    echo "-----------"
+    read -p "Entrez un choix" choix
+done
